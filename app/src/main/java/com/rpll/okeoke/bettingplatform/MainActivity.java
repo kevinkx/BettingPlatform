5be1e547f976e7e99b78bc0308689683f74f1c01
kevin <kevinkx55@gmail.com>
2018-03-01 10:53:56 +0700

    Initial commit

---
+++ app/src/main/java/com/rpll/okeoke/bettingplatform/MainActivity.java
@@ -0,0 +1,13 @@
+package com.rpll.okeoke.bettingplatform;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
