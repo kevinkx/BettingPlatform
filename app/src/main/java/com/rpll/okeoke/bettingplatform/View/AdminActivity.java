683fed1a65a9e8f7383ae17a9c25e8a04c166029
kevin <kevinkx55@gmail.com>
2018-04-25 12:45:32 +0700

    100% DONE v3

--- app/src/main/java/com/rpll/okeoke/bettingplatform/View/AdminActivity.java
+++ app/src/main/java/com/rpll/okeoke/bettingplatform/View/AdminActivity.java
@@ -3,17 +3,21 @@ package com.rpll.okeoke.bettingplatform.View;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.rpll.okeoke.bettingplatform.R;
 
 public class AdminActivity extends AppCompatActivity {
     private Button btnAdd,btnMatch,btnReport;
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+        auth = FirebaseAuth.getInstance();
         btnAdd = (Button) findViewById(R.id.btnAdd);
         btnMatch = (Button) findViewById(R.id.btnMatch);
         btnReport = (Button) findViewById(R.id.btnReport);
@@ -39,5 +43,18 @@ public class AdminActivity extends AppCompatActivity {
                 startActivity(myIntent);
             }
         });
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id== R.id.action_logout)
+        {
+            auth.signOut();
+            startActivity(new Intent(AdminActivity.this, FirstActivity.class));
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
